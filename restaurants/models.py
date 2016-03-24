5a4ae133d17b373c509ffc50a4464fb2d73825b0
Wildfoot <wild.foot.yee.tzwu@gmail.com>
2016-03-24 10:06:18 +0800

    model restaurant

--- restaurants/models.py
+++ restaurants/models.py
@@ -12,7 +12,7 @@ class Restaurant(models.Model):
 class Food(models.Model):
     name = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=3, decimal_places=0)
-    address = models.CharField(max_length=50, blank=True)
+    comment = models.CharField(max_length=50, blank=True)
     is_spicy = models.BooleanField(default=False)
     restaurant = models.ForeignKey(Restaurant)
 
--- restaurants/views.py
+++ restaurants/views.py
@@ -4,12 +4,13 @@ from django.http import HttpResponse
 #from django.template.loader import get_template
 from django.shortcuts import render_to_response
 from django import template
+from restaurants.models import Restaurant, Food
 	
 def menu(request):
-    food1 = {
-            'name' : '番茄炒蛋','price' : 60,'comment' : '好吃','is_spicy' : False}
-    food2 = {
-            'name' : '蒜泥白肉','price' : 100,'comment' : '人氣','is_spicy' : False}
-    foods = [food1, food2]
-
+#    food1 = {
+#            'name' : '番茄炒蛋','price' : 60,'comment' : '好吃','is_spicy' : False}
+#    food2 = {
+#            'name' : '蒜泥白肉','price' : 100,'comment' : '人氣','is_spicy' : False}
+#    foods = [food1, food2]
+    restaurants = Restaurant.objects.all()
     return render_to_response('menu.html', locals())
